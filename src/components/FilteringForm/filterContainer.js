110b4c7537a9b3bdb99b927faa6003c7c020f137
Karol Szymański <karol2134@op.pl>
2022-06-09 21:22:10 +0200

    Adding filterContainer.js

--- src/components/FilteringForm/filterContainer.js
+++ src/components/FilteringForm/filterContainer.js
@@ -1,5 +1,7 @@
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components'
-
+import TextInput from './FilteringInput/textInput';
+import FilterSubmit from "./FilteringButton/filterButton";
 const ContainerTitle = styled.h1`
 padding: 10px;
 `
@@ -7,16 +9,31 @@ padding: 10px;
 const Container = styled.div`
 
  width: 100%
- top: 10%; left: 5%;
+ height: 300px ;
+ background-color: black ;
 `;
 
 
 const FilterContainer = () =>
-(
+{
+    const [type, setType] = useState("");
+    const [title, setTitle] = useState("");
+
+
     <Container>
         <ContainerTitle>Filters</ContainerTitle>
+        <form>
+          <TextInput labelName={"Type:"} type={"text"} id={"type"} isFocus={true}  value={type} setValue={setType} />
+          <TextInput labelName={"Title contains:"} type={"text"} id={"titleContains"} isFocus={false}  value={title} setValue={setTitle} />
+          <div className="date-container">
+          </div>
+          <FilterSubmit title="Submit" />
+        </form>
     </Container>
-)
+}
+
+   
+
 
 
 export default FilterContainer;
