f1b727b8f79c805a560e952ef27c9c2e10da3f85
Karol Szymański <karol2134@op.pl>
2022-06-10 21:15:25 +0200

    Creating CardBook.js

---
+++ src/components/CardBook/CardBook.js
@@ -0,0 +1,24 @@
+
+import styled from 'styled-components'
+import { useGlobalState } from "../../GlobalState/globalState";
+const { useForm } = "ReactHookForm"
+
+
+
+
+const CardBook = styled.div`
+display: flex;
+font-weight: bold;
+list-style: none;
+a {
+    padding 5px;
+  }
+`
+
+const CardDescrption = styled.div`
+
+display: flex;
+flex-direction: column;
+
+
+
