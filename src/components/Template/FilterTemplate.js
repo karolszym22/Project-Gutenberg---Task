91c4f007f7b8d2979329c01038d698140cb0f899
Karol Szymański <karol2134@op.pl>
2022-06-10 21:16:09 +0200

    Refactoring: FilterContainer.js

--- src/components/Template/FilterTemplate.js
+++ src/components/Template/FilterTemplate.js
@@ -14,14 +14,18 @@ const [items, setItems] = useGlobalState('items');
 const [favoriteBooks, setFavoriteBooks] = useGlobalState('favoriteBooks');
 const [numberBooks, setNumberBooks] = useGlobalState('numberBooks');
 
+const fetchBooks = async () => {
+  const res = await fetch (`https://gnikdroy.pythonanywhere.com/api/book/?agent_alias_contains=&agent_birth_date_range_max=&agent_birth_date_range_min=&agent_death_date_range_max=&agent_death_date_range_min=&agent_name_contains=&agent_webpage_contains=&description_contains=&downloads_range_max=&downloads_range_min=&has_agent_type=&has_bookshelf=&has_resource_type=&languages=&title_contains=&type=`)
+  const data = await res.json();
+  setItems(data.results)
+  setNumberBooks(data.count)
+}
+
+
+
 useEffect(()=>{
-  const fetchBooks = async () => {
-    const res = await fetch (`https://gnikdroy.pythonanywhere.com/api/book/?agent_alias_contains=&agent_birth_date_range_max=&agent_birth_date_range_min=&agent_death_date_range_max=&agent_death_date_range_min=&agent_name_contains=&agent_webpage_contains=&description_contains=&downloads_range_max=&downloads_range_min=&has_agent_type=&has_bookshelf=&has_resource_type=&languages=&title_contains=&type=`)
-    const data = await res.json();
-    setItems(data.results)
-    setNumberBooks(data.count)
-  }
-  fetchBooks()
+  
+ fetchBooks()
   
 }, [])
 
@@ -34,12 +38,7 @@ useEffect(()=>{
         {items.map((item) => {
                return <div key={item.id}>{item.title}</div>
         })}
-
-        
-       
-        
-     
-     </div>
+       </div>
     );
   
   
